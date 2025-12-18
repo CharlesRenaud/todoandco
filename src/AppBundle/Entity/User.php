bfa5a3e41477c40253d75534f08d82709c02f721
CharlesRenaud <renaudcharlespro@gmail.com>
2025-12-18 18:09:34 +0100

    modif des entity user et task, des controller et des form ainsi que des templates twig concernés

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -35,23 +35,29 @@ class User implements UserInterface
     /**
      * @ORM\Column(type="string", length=60, unique=true)
      * @Assert\NotBlank(message="Vous devez saisir une adresse email.")
-     * @Assert\Email(message="Le format de l'adresse n'est pas correcte.")
+     * @Assert\Email(message="Le format de l'adresse n'est pas correct.")
      */
     private $email;
 
-    public function getId()
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = []; // stocke un tableau même si on accepte un rôle unique
+
+    public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getUsername()
+    public function getUsername(): ?string
     {
         return $this->username;
     }
 
-    public function setUsername($username)
+    public function setUsername(string $username): self
     {
         $this->username = $username;
+        return $this;
     }
 
     public function getSalt()
@@ -59,29 +65,39 @@ class User implements UserInterface
         return null;
     }
 
-    public function getPassword()
+    public function getPassword(): ?string
     {
         return $this->password;
     }
 
-    public function setPassword($password)
+    public function setPassword(?string $password): self
     {
-        $this->password = $password;
+        if ($password !== null) {
+            $this->password = $password;
+        }
+        return $this;
     }
 
-    public function getEmail()
+    public function getEmail(): ?string
     {
         return $this->email;
     }
 
-    public function setEmail($email)
+    public function setEmail(string $email): self
     {
         $this->email = $email;
+        return $this;
     }
 
-    public function getRoles()
+    public function getRoles(): array
     {
-        return array('ROLE_USER');
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+        return $this;
     }
 
     public function eraseCredentials()
--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -8,21 +8,38 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', TextType::class, ['label' => "Nom d'utilisateur"])
+            ->add('username', TextType::class, [
+                'label' => "Nom d'utilisateur",
+                'constraints' => [new NotBlank(['message' => 'Vous devez saisir un nom d\'utilisateur.'])]
+            ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'Les deux mots de passe doivent correspondre.',
-                'required' => true,
+                'required' => false, // facultatif pour l'édition
                 'first_options'  => ['label' => 'Mot de passe'],
                 'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
             ])
-            ->add('email', EmailType::class, ['label' => 'Adresse email'])
-        ;
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email',
+                'constraints' => [new NotBlank(['message' => 'Vous devez saisir une adresse email.'])]
+            ])
+            ->add('roles', ChoiceType::class, [
+                'choices'  => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                ],
+                'expanded' => true,
+                'multiple' => false,
+                'label' => 'Rôle',
+                'mapped' => false, // géré manuellement dans le controller
+            ]);
     }
 }
